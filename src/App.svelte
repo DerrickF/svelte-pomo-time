4b4b6e0b1e76faeca36b610d58a47e9243cd438c
Derrick Fink <DerrickFink@boisestate.edu>
2020-10-30 12:11:25 -0600

    chore: init repo

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+import Nested from "./Nested.svelte";
+
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>The creator and his proponents encourage a low-tech approach, using a mechanical timer, paper, and pencil.</p>
+	<p>The physical act of winding the timer confirms the user's determination to start the task;
+		ticking externalises desire to complete the task; ringing announces a break.
+	</p>
+	<p>
+		Flow and focus become associated with these physical stimuli
+	</p>
+	<Nested></Nested>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Nested.svelte
@@ -0,0 +1,26 @@
+<script>
+    let count = 1500000; // 25 mins
+    let task = '';
+
+    function start() {
+
+        window.document.title = count.toString();
+        let interval = setInterval(() => {
+            if(count > 0) {
+                count = count - 1;
+                window.document.title = count.toString();
+            } else {
+                clearInterval(interval);
+                window.alert('AAALLLL DONE')
+            }
+        }, 1000);
+    }
+</script>
+
+<h1>You Have {count}</h1>
+<h2>What are you going to work on ?</h2>
+<input bind:value={task}>
+
+<p>Start working on: {task}</p>
+
+<button on:click="{start}">START</button>
