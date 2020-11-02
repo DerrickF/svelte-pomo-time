e2ac20c6cfb91f63d5de72679b5ef473ba3a7163
Derrick Fink <DerrickFink@boisestate.edu>
2020-11-02 10:31:28 -0700

    fix: add check box

--- src/App.svelte
+++ src/App.svelte
@@ -4,19 +4,27 @@
   import List from 'smelte/src/components/List';
   import TextField from 'smelte/src/components/TextField';
   import dark from 'smelte/src/dark.js';
+  import Checkbox from 'smelte/src/components/Checkbox';
 
   const darkMode = dark();
   let text;
+  let uid = 1;
 
   let tasks = [
     {
+      id: uid++,
       text: 'Task 1',
+      done: false,
     },
     {
+      id: uid++,
       text: 'Task 2',
+      done: true,
     },
     {
+      id: uid++,
       text: 'Task 3',
+      done: false,
     },
   ];
 
@@ -24,6 +32,7 @@
     if (text.length > 3) {
       const newTask = {
         text: text,
+        id: uid++,
       };
       tasks = [...tasks, newTask];
       text = '';
@@ -63,12 +72,18 @@
 <main>
   <Button bind:value={$darkMode}>Toggle dark mode</Button>
   <h1>Task List</h1>
-  <TextField outlined="true" bind:value={text} style="max-width: 65%" />
+  <TextField
+    outlined="true"
+    bind:value={text}
+    style="max-width: 65%"
+    on:keydown={(e) => e.key === 'Enter' && addTask()} />
   <Button on:click={addTask}>Add</Button>
 
-  <List items={tasks}>
-    <li slot="item" let:item>
-      <span>
+  <div style="margin-top: 40px">
+    <h3>TODO:</h3>
+    <List items={tasks}>
+      <li slot="item" let:item>
+        <Checkbox value={item.done} />
         {item.text}
         <Button
           on:click={() => removeTask(item)}
@@ -78,7 +93,8 @@
           flat="true">
           <DeleteIcon size="24" />
         </Button>
-      </span>
-    </li>
-  </List>
+      </li>
+    </List>
+    <h3>DONE:</h3>
+  </div>
 </main>
